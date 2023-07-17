efb1e2f6100ce1802d13a18222bb0f34c321ef5b
Warren Ngoun <7400149+TidalWings@users.noreply.github.com>
2023-07-17 14:40:59 -0700

    Optimized data generation in main algorithm

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const io = require("socket.io")(server, { cors: { origin: "*" } });
 // Ensure API Keys and Confidential Data don't get published to Github
 const config = require("./private/keys.json");
 // Setting up a helper Wrapper library to make the Steam API much easier to use
-const steamWrapper = require('steam-js-api');
+const steamWrapper = require("steam-js-api");
 steamWrapper.setKey(config.steamKey);
 
 // Necessary for Steam Oauth
@@ -36,7 +36,7 @@ app.use(
     saveUninitialized: true,
   }),
   bodyParser.urlencoded({ extended: true }),
-  bodyParser.json({extended: true})
+  bodyParser.json({ extended: true })
 );
 
 // Tell Express which Templating Engine we're using
@@ -97,48 +97,52 @@ app.get("/room-choice", (req, res) => {
 
 //Passes host role
 app.post("/room-choice", async (req, res) => {
-  let roomNumber = Math.floor(Math.random()*90000) + 10000;
+  let roomNumber = Math.floor(Math.random() * 90000) + 10000;
   roomNumber = roomNumber.toString();
-    // Ensures that room numbers are random and unique so we don't have colliding room IDs.
+  // Ensures that room numbers are random and unique so we don't have colliding room IDs.
   while (existingRooms.includes(roomNumber)) {
-    roomNumber = Math.floor(Math.random()*90000) + 10000;
+    roomNumber = Math.floor(Math.random() * 90000) + 10000;
     roomNumber = roomNumber.toString();
   }
 
-// Add our now guaranteed unique room to the existing rooms & also add the number to the users cookies.
+  // Add our now guaranteed unique room to the existing rooms & also add the number to the users cookies.
   existingRooms.push(roomNumber);
   res.cookie("roomNumber", roomNumber);
 
-// Render the next page for the Host now with the number on their page.
-  res.redirect("empty-room", {
-    role: req.body.role,
-    roomNumber: this.roomNumber,
-    url: config.url,
-  }, 303);
+  // Render the next page for the Host now with the number on their page.
+  res.redirect(
+    "empty-room",
+    {
+      role: req.body.role,
+      roomNumber: this.roomNumber,
+      url: config.url,
+    },
+    303
+  );
 });
 
 app.get("/join-room", async (req, res) => {
-  res.render("join-room", {existingRooms: existingRooms});
-}); 
+  res.render("join-room", { existingRooms: existingRooms });
+});
 
 app.post("/join-room", (req, res) => {
-    let potentialRoomNum = req.body.roomnum;
-    // DEBUG: Check the incoming data and the struct it's being compared to
-    // console.log(`${potentialRoomNum}`);
-    // console.log(existingRooms);
-    if (existingRooms.includes(potentialRoomNum)) {
-        console.log(`Room FOUND`);
-        res.cookie("roomNumber", potentialRoomNum);
-        res.render("empty-room", {roomNumber: potentialRoomNum, url: config.url});
-    } else {
-        console.log(`Room NOT FOUND`);
-        res.render("join-room", {existingRooms: existingRooms});
-    }
+  let potentialRoomNum = req.body.roomnum;
+  // DEBUG: Check the incoming data and the struct it's being compared to
+  // console.log(`${potentialRoomNum}`);
+  // console.log(existingRooms);
+  if (existingRooms.includes(potentialRoomNum)) {
+    console.log(`Room FOUND`);
+    res.cookie("roomNumber", potentialRoomNum);
+    res.render("empty-room", { roomNumber: potentialRoomNum, url: config.url });
+  } else {
+    console.log(`Room NOT FOUND`);
+    res.render("join-room", { existingRooms: existingRooms });
+  }
 });
 
 // TODO: Ensure that regardless of the proper routing, that all pages validate and ensure they have the data they need (e.g. empty-room will redirect the users to create/join room if they DONT have a Room Number in their cookies).
 app.get("/empty-room", (req, res) => {
-    // console.log(req.cookies);
+  // console.log(req.cookies);
   res.render("empty-room", {
     roomNumber: req.cookies.roomNumber,
     url: config.url,
@@ -182,34 +186,36 @@ io.on("connection", (socket) => {
     let roomNumber = data.roomNumber;
     socket.join("room-" + roomNumber);
 
-    let potentialRoom = socketRooms.find(x => x.roomNumber === roomNumber);
+    let potentialRoom = socketRooms.find((x) => x.roomNumber === roomNumber);
     // Using the variable above, we can check if there IS a room or not
-    if (typeof potentialRoom != 'undefined') {
-        // DEBUG: Checking our Logic
-        console.log(`Found Room: ${roomNumber}`);
-        let foundMembers = potentialRoom.roomMembers;
-        // IF the USER is ALREADY there DONT update
-        let hasFound = false;
-        for (let i = 0; i < foundMembers.length; i++) {
-            if (foundMembers[i][0] == data.steamID) {
-                hasFound = true;
-            }
+    if (typeof potentialRoom != "undefined") {
+      // DEBUG: Checking our Logic
+      console.log(`Found Room: ${roomNumber}`);
+      let foundMembers = potentialRoom.roomMembers;
+      // IF the USER is ALREADY there DONT update
+      let hasFound = false;
+      for (let i = 0; i < foundMembers.length; i++) {
+        if (foundMembers[i][0] == data.steamID) {
+          hasFound = true;
         }
-        
-        if (hasFound == false) {
-            foundMembers.push([data.steamID, data.username, data.avatar]);
-            potentialRoom.roomMembers = foundMembers;
-        }
+      }
+
+      if (hasFound == false) {
+        foundMembers.push([data.steamID, data.username, data.avatar]);
+        potentialRoom.roomMembers = foundMembers;
+      }
     } else {
-        // DEBUG: Checking our Logic
-        console.log(`Room NOT Found: ${roomNumber}`);
-        // Made a temp array to store the first user (HOST) and add to the array keeping track of existing socket rooms.
-        let temp = new Room(roomNumber, [[data.steamID, data.username, data.avatar]]);
-        socketRooms.push(temp);
+      // DEBUG: Checking our Logic
+      console.log(`Room NOT Found: ${roomNumber}`);
+      // Made a temp array to store the first user (HOST) and add to the array keeping track of existing socket rooms.
+      let temp = new Room(roomNumber, [
+        [data.steamID, data.username, data.avatar],
+      ]);
+      socketRooms.push(temp);
     }
-    
+
     // Refind the room again and set the output of users to the front-end
-    potentialRoom = socketRooms.find(x => x.roomNumber === roomNumber);
+    potentialRoom = socketRooms.find((x) => x.roomNumber === roomNumber);
     roomMembers = potentialRoom.roomMembers;
 
     io.sockets.in("room-" + roomNumber).emit("otherMsg", roomMembers);
@@ -223,72 +229,54 @@ io.on("connection", (socket) => {
   // MAIN WORKHORSE FUNCTION. Gathers the SteamIDs of the room members and uses them to generate the massive list of shared games.
   // Sort by amount of time played and then generate shared list
   socket.on("generate", async (data) => {
-    let roomNumber = data.roomNumber;
-    let roomMembers = socketRooms.find(x => x.roomNumber === roomNumber).roomMembers;
-
-    function Game(gameName, gameImage, gameLink) {
-        this.gameName = gameName;
-        this.gameImage = gameImage;
-        this.gameLink = gameLink;
-    }
-
-    // We first start by gathering and generating all the games of each member
-    // roomMembersGames will be a 2D array with each index being another array of all the games of that user.
-    let roomMembersGames = [];
-
-    // TODO: For efficiency sake, this essentially runs TWO major for-loops. One to build the data, and the second to process it. Can combine both into one to not waste computational space.
-
-    for (let i = 0; i < roomMembers.length; i++) {
-
-        let allInfo = [];
-        let gameCount = 0;
-    
-        let curMembersID = roomMembers[i][0];
-
-        await steamWrapper.getOwnedGames(curMembersID, null, true).then(result => {
-            gameCount = result.data.count;
-            allInfo = result.data.games;
-        }).catch(console.error);
-
-      // Build the temporary sub-array to be pushed at the end.
-      let tempUserGames = [];
-    // Run through each game for the relevant user and build their "libraries" to run through later.
-      for (let j = 0; j < gameCount; j++) {
-        // TODO: Here is where we could filter out games before they're added into each users array.
-
-        let gameID = allInfo[j].appID;
-
-        let gamePic = allInfo[j].url_store_header;
-        
-        let tempGame = new Game(allInfo[j].name, 
-            gamePic, 
-            allInfo[j].url_store);
-        tempUserGames.push(tempGame);
-      };
-      roomMembersGames.push(tempUserGames);
-    }
-
+    const roomNumber = data.roomNumber;
+    const roomMembers = socketRooms.find(
+      (x) => x.roomNumber === roomNumber
+    ).roomMembers;
+    // Arrays to be sent to the front-end later.
     let sharedGameNames = [];
     let ownedByWho = [];
     let gameImages = [];
     let gameLinks = [];
+    // let gameTags = []; // TODO: Add Tag functionality
 
-    // Will iterate through EVERY game in ALL members libraries so could possibly take some time (does the operation in O(m+n...) time where each X is the size of another users library).
-    for (let i = 0; i < roomMembersGames.length; i++) {
-      for (let j = 0; j < roomMembersGames[i].length; j++) {
-        let currentGame = roomMembersGames[i][j];
+    // First we'll iterate through EVERY room member. Goal is to run through each user and their games and "tick" off who owns what. 
+    for (let i = 0; i < roomMembers.length; i++) {
+      // A users total game count & an array of their games. For iterating & that'll be set later.
+      let gameInfo = [];
+      let gameCount = 0;
+      // aka their SteamID.
+      const curMembersID = roomMembers[i][0];
+
+      // An API function that will set gameCount and gameInfo to the total count of a users games and an array of their games respectively.
+      await steamWrapper
+        .getOwnedGames(curMembersID, null, true)
+        .then((result) => {
+          gameCount = result.data.count;
+          gameInfo = result.data.games;
+        })
+        .catch(console.error);
+
+      // Now we can iterate through the CURRENT USERS GAMES using the data from the above function.
+      for (let curGame = 0; curGame < gameCount; curGame++) {
+        const gameName = gameInfo[curGame].name;
+        const gamePic = gameInfo[curGame].url_store_header;
+        const gameURL = gameInfo[curGame].url_store;
+
+        // TODO: Here is where we could filter out games before they're added into each users array.
 
         // Checking if the current game is in out checked list or not
-        let indexOfGame = sharedGameNames.indexOf(currentGame.gameName);
+        const indexOfGame = sharedGameNames.indexOf(gameName);
+        // TODO: Would it be better to use a games ID here? Can a game have the same name as another?
         if (indexOfGame != -1) {
-            // It IS THERE so just append the SteamID to the "current owners"
+          // It IS THERE so curGame append the SteamID to the "current owners"
           ownedByWho[indexOfGame].push(i);
         } else {
           // it IS NOT there so make a new entry with name, image, & link
-          sharedGameNames.push(currentGame.gameName);
-          gameImages.push(currentGame.gameImage);
-          gameLinks.push(currentGame.gameLink);
-            // Add the SteamID to a new array and start the appending process
+          sharedGameNames.push(gameName);
+          gameImages.push(gamePic);
+          gameLinks.push(gameURL);
+          // Add the SteamID to a new array and start the appending process
           let temp = [];
           temp.push(i);
           ownedByWho.push(temp);
@@ -297,15 +285,13 @@ io.on("connection", (socket) => {
     }
 
     socket.join("room-" + roomNumber);
-    io.sockets
-      .in("room-" + roomNumber)
-      .emit("finalList", {
-        roomMembers: roomMembers,
-        games: sharedGameNames,
-        owners: ownedByWho,
-        images: gameImages,
-        links: gameLinks,
-      });
+    io.sockets.in("room-" + roomNumber).emit("finalList", {
+      roomMembers: roomMembers,
+      games: sharedGameNames,
+      owners: ownedByWho,
+      images: gameImages,
+      links: gameLinks,
+    });
   });
 });
 
