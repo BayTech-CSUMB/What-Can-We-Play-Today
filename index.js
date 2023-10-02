a24cd4d66b4699bd639a3cc48ce930e94aaf16b4
Warren Ngoun <7400149+TidalWings@users.noreply.github.com>
2023-10-02 16:11:13 -0700

    Major Routing Validation
    Ensures most routes are protected and that routes shouldn't be accessed at improper areas (e.g. User trying to access List w/out even signing into Steam yet). Fixes roomMembers bug by making sure users shouldn't be able to access the List page without proper information.

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 // Critical for Express itself
 const express = require("express");
 const app = express();
-const fs = require('fs');
 
 // Ensure API Keys and Confidential Data don't get published to Github
 const config = require("./private/keys.json");
 
-const options = {
-  key: fs.readFileSync(config.keyPath),
-  cert: fs.readFileSync(config.certPath)
-};
-
-// require('./server.js');
+// UNCOMMENT FOR PROD
+// const fs = require("fs");
+// const options = {
+//   key: fs.readFileSync(config.keyPath),
+//   cert: fs.readFileSync(config.certPath)
+// };
 
 // Modules used to facilitate data transfer and storage
 const cookieParser = require("cookie-parser");
@@ -19,8 +18,13 @@ app.use(cookieParser());
 const bodyParser = require("body-parser");
 const fetch = require("node-fetch");
 const axios = require("axios");
+const moment = require("moment");
 
-const server = require("https").createServer(options, app);
+// UNCOMMENT FOR PROD
+// const server = require("https").createServer(options, app);
+// UNCOMMENT FOR DEV
+const server = require("http").createServer(app);
+
 // TODO: Double check what CORS policy will mean for our app.
 const io = require("socket.io")(server, { cors: { origin: "*" } });
 
@@ -33,25 +37,19 @@ const SteamAuth = require("node-steam-openid");
 
 // Setup for Steam Oauth
 const steam = new SteamAuth({
-  // TODO: Eventually this will be set to the proper Domain name.
   realm: config.url,
   returnUrl: config.url + "/auth/steam/authenticate",
   apiKey: config.steamKey,
 });
 
+// Ensure this doesn't negatively impact before final removal.
 // Setup for keeping track of Users temporary data.
-const session = require("express-session");
-const { types } = require("util");
-const { parse } = require("path");
-app.use(
-  session({
-    secret: config.sessionSecret,
-    resave: true,
-    saveUninitialized: true,
-  }),
-  bodyParser.urlencoded({ extended: true }),
-  bodyParser.json({ extended: true })
-);
+// const { types } = require("util");
+// const { parse } = require("path");
+// app.use(
+//   bodyParser.urlencoded({ extended: true }),
+//   bodyParser.json({ extended: true })
+// );
 
 // Tell Express which Templating Engine we're using
 app.set("view engine", "ejs");
@@ -74,8 +72,20 @@ function Room(roomNumber, roomMembers) {
 }
 
 // Hard coded users for testing
-let mainTestingRoom = new Room(`99999`, [[`76561198016716226`, `TidalWings`, `https://avatars.steamstatic.com/de63198a51c76b3ed2dfd72e82d2ce4d666ce449_medium.jpg`]]);
-let altTestingRoom = new Room(`11111`, [[`76561199516233321`, `drslurpeemd`, `https://avatars.steamstatic.com/b9fa08a1e25a9dadaebbab031b6b2974502416fa_medium.jpg`]]);
+let mainTestingRoom = new Room(`99999`, [
+  [
+    `76561198016716226`,
+    `TidalWings`,
+    `https://avatars.steamstatic.com/de63198a51c76b3ed2dfd72e82d2ce4d666ce449_medium.jpg`,
+  ],
+]);
+let altTestingRoom = new Room(`11111`, [
+  [
+    `76561199516233321`,
+    `drslurpeemd`,
+    `https://avatars.steamstatic.com/b9fa08a1e25a9dadaebbab031b6b2974502416fa_medium.jpg`,
+  ],
+]);
 let socketRooms = [];
 socketRooms.push(mainTestingRoom);
 socketRooms.push(altTestingRoom);
@@ -95,57 +105,57 @@ async function fetchTags(gameID) {
 
 /**
  * deleteRoom will only be run on situations where the user is the LAST user in a room. It will go and delete the entire element from the array.
- * @param {Number} roomNumber the number of the room to be deleted 
+ * @param {Number} roomNumber the number of the room to be deleted
  */
 function deleteRoom(roomNumber) {
-    let indexOfRoom = -1;
+  let indexOfRoom = -1;
 
-    for (let i = 0; i < socketRooms.length; i++) {
-        const curRoomNum = socketRooms[i].roomNumber;
-        if (curRoomNum == roomNumber) {
-            indexOfRoom = i;
-        }
+  for (let i = 0; i < socketRooms.length; i++) {
+    const curRoomNum = socketRooms[i].roomNumber;
+    if (curRoomNum == roomNumber) {
+      indexOfRoom = i;
     }
+  }
 
-    // Ensuring we ACTUALLY only delete the room if it's found
-    if (indexOfRoom != -1) {
-        socketRooms.splice(indexOfRoom, 1);
-    }
+  // Ensuring we ACTUALLY only delete the room if it's found
+  if (indexOfRoom != -1) {
+    socketRooms.splice(indexOfRoom, 1);
+  }
 }
 
 /**
  * Removes user from the room
- * @param {*} roomNumber 
- * @param {*} userID 
+ * @param {*} roomNumber
+ * @param {*} userID
  */
 function deleteUserFromRoom(roomNumber, userID) {
-    let indexOfRoom = -1;
+  let indexOfRoom = -1;
 
-    for (let i = 0; i < socketRooms.length; i++) {
-        const curRoomNum = socketRooms[i].roomNumber;
-        if (curRoomNum == roomNumber) {
-            indexOfRoom = i;
-        }
+  for (let i = 0; i < socketRooms.length; i++) {
+    const curRoomNum = socketRooms[i].roomNumber;
+    if (curRoomNum == roomNumber) {
+      indexOfRoom = i;
+    }
+  }
+
+  // Ensuring we ACTUALLY only delete the room if it's found
+  if (indexOfRoom != -1) {
+    let indexOfUser = -1;
+
+    let curRoomMembers = socketRooms[indexOfRoom].roomMembers;
+    for (let j = 0; j < curRoomMembers.length; j++) {
+      if (curRoomMembers[j][0] == userID) {
+        indexOfUser = j;
+      }
     }
 
-    // Ensuring we ACTUALLY only delete the room if it's found
-    if (indexOfRoom != -1) {
-        let indexOfUser = -1;
-        
-        let curRoomMembers = socketRooms[indexOfRoom].roomMembers;
-        for (let j = 0; j < curRoomMembers.length; j++) {
-            if (curRoomMembers[j][0] == userID) {
-                indexOfUser = j;
-            }
-        }
-
-        if (indexOfUser != -1) {
-            socketRooms[indexOfRoom].roomMembers.splice(indexOfUser, 1);
-        }
+    if (indexOfUser != -1) {
+      socketRooms[indexOfRoom].roomMembers.splice(indexOfUser, 1);
     }
+  }
 }
 
-// TODO: Make this dynamically generate a YYYY-MM-DD format
+// TODO: Make this dynamically generate a YYYY-MM-DD format.
 function generateDate() {
   return `2023-07-20`;
 }
@@ -237,7 +247,7 @@ async function checkGames(steamID) {
     if (localGame) {
       // IFF >= 3 days old then re-query
       if (computeDateDiff(localGame.age)) {
-        // TODO:
+        // TODO: This entire thing.
       }
       // If they don't have the game in their table we add it to their database else do nothing
       if (!userPotentialGame) {
@@ -338,7 +348,8 @@ app.get("/auth/steam/authenticate", async (req, res) => {
 
     res.render("room-choice");
   } catch (error) {
-    console.error(`ERROR: Couldn't Fetch! ${error}`);
+    const tempTime = moment.toString();
+    console.error(`ERROR: Couldn't Fetch! ${error}; ${tempTime}`);
   }
 });
 
@@ -349,10 +360,14 @@ app.get("/alt-login", (req, res) => {
 
 // Users get shown the CREATE or JOIN room buttons. Here they'll start the process of generating a Room Number and allowing others to join them.
 app.get("/room-choice", (req, res) => {
-  res.render("room-choice");
+  if (req.cookies.steamID == null) {
+    res.redirect("/");
+  } else {
+    res.render("room-choice");
+  }
 });
 
-//Passes host role and creates a unique room
+// Passes host role and creates a unique room
 app.post("/room-choice", async (req, res) => {
   let roomNumber = Math.floor(Math.random() * 90000) + 10000;
   roomNumber = roomNumber.toString();
@@ -380,7 +395,11 @@ app.post("/room-choice", async (req, res) => {
 
 // Renders join room page with all the currently existing rooms
 app.get("/join-room", (req, res) => {
-  res.render("join-room", { existingRooms: existingRooms });
+  if (req.cookies.steamID == null) {
+    res.redirect("/");
+  } else {
+    res.render("join-room", { existingRooms: existingRooms });
+  }
 });
 
 // Gets data from front end and then adds user to the room if the code is valid
@@ -390,28 +409,35 @@ app.post("/join-room", async (req, res) => {
 
   if (existingRooms.includes(potentialRoomNum)) {
     res.cookie("roomNumber", potentialRoomNum);
-    res.render("empty-room", { roomNumber: potentialRoomNum, url: config.url});
+    res.render("empty-room", { roomNumber: potentialRoomNum, url: config.url });
   } else {
-    res.render("join-room", { existingRooms: existingRooms});
+    res.render("join-room", { existingRooms: existingRooms });
   }
 });
 
 // Renders list page
 app.get("/list", async (req, res) => {
-
-  res.render("list", { url: config.url});
+  if (req.cookies.roomNumber == null && req.cookies.steamID == null) {
+    res.redirect("/");
+  } else if (req.cookies.roomNumber == null) {
+    res.redirect("/room-choice");
+  } else {
+    res.render("list", { url: config.url });
+  }
 });
 
-// TODO: Ensure that regardless of the proper routing, that all pages validate and ensure they have the data they need (e.g. empty-room will redirect the users to create/join room if they DONT have a Room Number in their cookies).
 // Renders the room with its users
 app.get("/empty-room", async (req, res) => {
-    
-  await checkGames(req.cookies.steamID);
+  if (req.cookies.steamID == null) {
+    res.redirect("/");
+  } else {
+    await checkGames(req.cookies.steamID);
 
-  res.render("empty-room", {
-    roomNumber: req.cookies.roomNumber,
-    url: config.url
-  });
+    res.render("empty-room", {
+      roomNumber: req.cookies.roomNumber,
+      url: config.url,
+    });
+  }
 });
 
 //Used for alt login
@@ -437,18 +463,16 @@ app.post("/alt-login", async (req, res) => {
   }
 });
 
-
 //Socket.io used to room member data to the front end
 io.on("connection", (socket) => {
-
   // Used to refresh the list when a user joins/leaves a room
-  socket.on('generateList', (data) => {
+  socket.on("generateList", (data) => {
     socket.join("room-" + data.roomNumber);
     io.sockets.in("room-" + data.roomNumber).emit("refreshList");
   });
 
   // Used to refresh the empty-room page when a user leaves
-  socket.on('generateList2', (data) => {
+  socket.on("generateList2", (data) => {
     socket.join("room-" + data.roomNumber);
     io.sockets.in("room-" + data.roomNumber).emit("refreshList2");
   });
@@ -487,7 +511,6 @@ io.on("connection", (socket) => {
       ]);
       socketRooms.push(temp);
     }
-
 
     // Re-find the room again and send the output of users to the front-end
     potentialRoom = socketRooms.find((x) => x.roomNumber === roomNumber);
@@ -563,11 +586,9 @@ io.on("connection", (socket) => {
         // TODO: Would it be better to use a games ID here? Can a game have the same name as another?
         const indexOfGame = sharedGameNames.indexOf(curGame.name);
         if (indexOfGame != -1) {
-
           // It IS THERE so curGame append the SteamID to the "current owners"
           ownedByWho[indexOfGame].push(i);
         } else {
-
           // It IS NOT there so make a new entry with name, image, & link
           sharedGameNames.push(curGame.name);
           gameImages.push(curGame.header_image);
@@ -589,6 +610,8 @@ io.on("connection", (socket) => {
 
           // Lets process the tags to send to the front end
           // TODO: We should probably also remove the current (or previously) selected tags so they don't get queried again from the database.
+          // TODO: Add ability for multiple tags to be selected. For example, of
+          // all the games, only show NEW UNSELECTED tags.
           allPotentialTags = maintainTags(curGame.tags, allPotentialTags);
         }
       });
@@ -597,7 +620,6 @@ io.on("connection", (socket) => {
       socket.join("room-" + data.roomNumber);
     }
 
-    // TODO: How to handle refreshes when a new user joins or leaves a room?
     // Finally emit the data to all room members INDIVIDUALLY so filtering options don't change the page for everyone.
     io.to(socket.id).emit("finalList", {
       roomMembers: roomMembers,
@@ -609,19 +631,17 @@ io.on("connection", (socket) => {
       prices: gamePrices,
       categories: allPotentialTags,
     });
-    
   });
-
 });
-
 
 // DEBUG: For checking HTML elements on a safe page.
 app.get("/test", async (req, res) => {
   console.log(`Running Test.`);
-
-    console.log(socketRooms);
-    // console.log(socketRooms[0].roomMembers);
-    // console.log(socketRooms[0].roomNumber);
+  // console.log(socketRooms);
+  // console.log(socketRooms[0].roomMembers);
+  // console.log(socketRooms[0].roomNumber);
+  const tempTime = moment().toString();
+  console.log(`ERROR: Failed something idk; ${tempTime}`);
 
   res.render("test");
 });
@@ -630,156 +650,86 @@ app.get("/test", async (req, res) => {
 app.get("/altTest", async (req, res) => {
   console.log("Checking for new games to add...");
 
-  let gameInfo = [];
-  let gameCount = 0;
   // Set this to whomever's account to pre-add their games to the database
   let steamID = `76561198016716226`;
-
-  // An API function that will set gameCount and gameInfo to the total count of a users games and an array of their games respectively.
-  await steamWrapper
-    .getOwnedGames(steamID, null, true)
-    .then((result) => {
-      gameCount = result.data.count;
-      gameInfo = result.data.games;
-    })
-    .catch(console.error);
-
-  // gameCount = 600;
-
-  // We iterate through the users' games using the data from the above function
-  for (let curGame = 0; curGame < gameCount; curGame++) {
-    const gameName = gameInfo[curGame].name;
-    const gamePic = gameInfo[curGame].url_store_header;
-    const gameURL = gameInfo[curGame].url_store;
-    const gameID = gameInfo[curGame].appID;
-
-    // Variables that are et later with API fetches
-    let tags = "";
-    let genre = "";
-    let final_price = 0;
-    let initial_price = 0;
-
-    // FIRST we query our database to see if we HAVE the game or not
-    const localGame = db
-      .prepare("SELECT * FROM Games WHERE gameID = ?")
-      .get(`${gameID}`);
-
-    // Then check if the user has the local game registered in the database
-    const userPotentialGame = db
-      .prepare("SELECT * FROM Users WHERE userID = ? AND gameID = ?")
-      .get([`${steamID}`, `${gameID}`]);
-
-    // if the game is located we check if the user has the game in their database
-    if (localGame) {
-      // IFF >= 3 days old then re-query
-      if (computeDateDiff(localGame.age)) {
-        // TODO:
-      }
-      // If they don't have the game in their table we add it to their database else do nothing
-      if (!userPotentialGame) {
-        db.prepare(`INSERT INTO Users (userID, gameID) VALUES (?, ?)`).run(
-          steamID,
-          `${gameID}`
-        );
-      }
-    } else {
-      // Case if the game is not located in the database
-      // We query game and add it to the Games table along with the users personal table
-      tags = await fetchTags(gameID);
-      let temp = await fetchGenresPrices(gameID);
-      genre = temp[0];
-      initial_price = temp[1];
-      final_price = temp[2];
-      let is_multiplayer = 1;
-      let age = generateDate();
-
-      // If its single player
-      if (!genre.includes(`Multi-player`)) {
-        is_multiplayer = 0;
-      }
-
-      console.log(gameID);
-      db.prepare(
-        `INSERT INTO Games(gameID, name, genre, tags, age, price, initial_price, is_multiplayer, header_image, store_url) VALUES (?,?,?,?,?,?,?,?,?,?)`
-      ).run(
-        `${gameID}`,
-        gameName,
-        genre,
-        tags,
-        age,
-        final_price,
-        initial_price,
-        `${is_multiplayer}`,
-        gamePic,
-        gameURL
-      );
-
-      db.prepare(`INSERT INTO Users (userID, gameID) VALUES (?,?)`).run(
-        steamID,
-        `${gameID}`
-      );
-    }
-  }
-
-  console.log("Finished adding games!");
-
+  // Note: re-copy the code from the function and modify it
   res.render("altTest");
 });
 
 // Intended to be the FULL logout from Steam & the Room.
 app.get("/logout", (req, res) => {
-    const roomNumber = req.cookies.roomNumber;
-    const curUsersID = req.cookies.steamID;
-    let potentialRoom = socketRooms.find((x) => x.roomNumber === roomNumber);
-    
-    if (potentialRoom) { // our room was found so delete the user
-        if (potentialRoom.roomMembers.length == 1) { // user is the sole person in the room
-            deleteRoom(roomNumber);
-            // otherwise they're in a populated room and need to be specifically removed
-        } else {
-            deleteUserFromRoom(roomNumber, curUsersID);
-        }
-    } else {
-        console.error(`ERROR: User was in a room that doesn't exist anymore?`);
-    }
+  const roomNumber = req.cookies.roomNumber;
+  const curUsersID = req.cookies.steamID;
+  let potentialRoom = socketRooms.find((x) => x.roomNumber === roomNumber);
 
-    // After removing them from a room, we'll clear their cookies.
+  if (potentialRoom) {
+    // our room was found so delete the user
+    if (potentialRoom.roomMembers.length == 1) {
+      // user is the sole person in the room
+      deleteRoom(roomNumber);
+      // otherwise they're in a populated room and need to be specifically removed
+    } else {
+      deleteUserFromRoom(roomNumber, curUsersID);
+    }
+  } else {
+    const tempTime = moment.toString();
+    console.error(
+      `ERROR: User was in a room that doesn't exist anymore? ${tempTime}`
+    );
+  }
+
+  // After removing them from a room, we'll clear their cookies.
   res.clearCookie("steamID");
   res.clearCookie("username");
   res.clearCookie("avatar");
   res.clearCookie("roomNumber");
 
-  res.render("index");
+  res.render("alt-index");
 });
 
 // Same functionality as logout but only clear the roomNumber
-app.get('/leave', (req, res) => {
-  const roomNumber = req.cookies.roomNumber;
-  const curUsersID = req.cookies.steamID;
-  let potentialRoom = socketRooms.find((x) => x.roomNumber === roomNumber);
-  
-  if (potentialRoom) { // our room was found so delete the user
-      if (potentialRoom.roomMembers.length == 1) { // user is the sole person in the room
-          deleteRoom(roomNumber);
-          // otherwise they're in a populated room and need to be specifically removed
-      } else { 
-          deleteUserFromRoom(roomNumber, curUsersID);
-      }
+app.get("/leave", (req, res) => {
+  if (req.cookies.steamID == null) {
+    res.redirect("/");
   } else {
-      console.error(`ERROR: User was in a room that doesn't exist anymore?`);
+    const roomNumber = req.cookies.roomNumber;
+    const curUsersID = req.cookies.steamID;
+    let potentialRoom = socketRooms.find((x) => x.roomNumber === roomNumber);
+
+    if (potentialRoom) {
+      // our room was found so delete the user
+      if (potentialRoom.roomMembers.length == 1) {
+        // user is the sole person in the room
+        deleteRoom(roomNumber);
+        // otherwise they're in a populated room and need to be specifically removed
+      } else {
+        deleteUserFromRoom(roomNumber, curUsersID);
+      }
+    } else {
+      const tempTime = moment.toString();
+      console.error(
+        `ERROR: User was in a room that doesn't exist anymore? ${tempTime}`
+      );
+    }
+
+    res.clearCookie("roomNumber");
+    res.render("room-choice");
   }
-
-  res.clearCookie('roomNumber');
-
-  res.render('room-choice');
 });
+
+// TODO: Make this look like a 404 Page, not super important rn though.
+app.get('*', (req, res) => {
+    res.send("Error 404: Hit Back on your Browser. You shouldn't be here.");
+});
+
+// ======== SERVER LINES ========
 
 // Starts server
 // server.listen(80, () => {
 //   console.log(`SocketIO Server has Started!`);
 // });
- 
-// TODO: Confirm weird HTTP issues
+
+// TODO: Confirm weird HTTP issues and or attempt to re-route port 80 traffic to 443
 server.listen(443, () => {
-  console.log('HTTPS server running on port 443');
+  console.log("HTTPS server running on port 443");
 });
